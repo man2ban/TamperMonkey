cd2cc1d59270647c40083aacc8af3dfdffa0e20f
Gaspard Bettinger <Xzefiri@live.fr>
2020-05-15 14:01:34 +0200

    changed version

--- e621 scraping test.js
+++ e621 scraping test.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         e621 scraping test
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  try to take over the world!
 // @author       AyOne
 // @grant        none
