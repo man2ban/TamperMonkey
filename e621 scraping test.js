3816dba4002137c2c2234bee6f6b52051d662c71
Gaspard Bettinger <Xzefiri@live.fr>
2020-05-15 13:46:57 +0200

    initial commit

---
+++ e621 scraping test.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         e621 scraping test
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       AyOne
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.5.1.min.js
+// @include      https://e621.net/*
+// ==/UserScript==
+
+
+const testing_url = "https://e621.net/posts?tags=rating%3Aq";
+
+
+(function() {
+    alert("Hello");
+    $.ajax({ url: testing_url, success: function(data) {
+
+    }});
+})();
